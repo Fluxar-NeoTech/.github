c15eaf19c1f8f54f27db6458761ee0a8334e0fd5
DudaCacciatore <158073263+DudaCacciatore@users.noreply.github.com>
2025-08-21 23:15:04 -0300

    Create README.m
    Teste

---
+++ profile/README.m
@@ -0,0 +1,51 @@
+
+# NeoTech - Fluxar
+
+📍A NeoTech nasceu em 2025, como resultado de um projeto interdisciplinar escolar. Nesse processo, tivemos contato direto com fábricas, comerciantes e realizamos diversas pesquisas, o que nos permitiu identificar falhas e dificuldades na organização e no gerenciamento de estoques em diferentes contextos.
+
+💡 Dessa experiência surgiu o Fluxar, um aplicativo desenvolvido para auxiliar empresas alimentícias de qualquer porte no controle de seus estoques. Nossa missão é apoiar essas indústrias na busca constante por otimização e melhorias em suas operações, tornando seus processos mais ágeis, eficientes e sustentáveis.
+
+
+## Desenvolvimento 🛠️
+
+![HTML](.github\profile\images\Html_Icon.png)
+![CSS](.github\profile\images\Html_CSS_Icon.png)
+![JavaScript](.github\profile\images\JS_Icon.png)
+![React](.github\profile\images\React_Icon.png)
+![Java](.github\profile\images\Java_Icon.png)
+![Spring Boot](.github\profile\images\Sprint_Boot_Icon.png)
+![Kotlin](.github\profile\images\Kotlin_Icon.png)
+![Python](.github\profile\images\Python_Icon.png)
+![MongoDB](.github\profile\images\MongoDB_Icon.png)
+![Redis](.github\profile\images\Redis_Icon.png)
+![PostgreSQL](.github\profile\images\PostgreSQL_Icon.png)
+![Cloudnary](.github\profile\images\Cloudnary_Icon.png)
+
+
+
+
+## Autores 🖥️
+
+O projeto foi desenvolvido por três equipes principais: os desenvolvedores de sistema, os analistas de dados e o grupo responsável por idealizar e implementar a proposta do Fluxar.
+
+
+## Documentação 📄
+Para acessar documentações mais detalhadas e obter melhor entendimento sobre a construção do projeto, acesse o README dos repositórios: 
+
+- [WebSite](https://github.com/Fluxar-NeoTech/WebSite-Fluxar)
+- [App](https://github.com/Fluxar-NeoTech/App-Fluxar)
+- [ChatBot](https://github.com/Fluxar-NeoTech/ChatBot-Fluxar)
+- [API](https://github.com/Fluxar-NeoTech/API-Fluxar)
+
+
+
+
+## Acesso ao Fluxar 🚀
+
+Para usar o aplicativo Fluxar, a empresa deve assinar um dos planos de serviço da NeoTech. Após a contratação, todos os gestores e analistas podem usar o aplicativo livremente.
+
+
+## Suporte 🤝
+
+Para suporte, mande um email para suporte2025.neo.tech@gmail.com.
+
